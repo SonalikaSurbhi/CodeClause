be7dc9009dc07df39aaf3f51ed7d70323e0b2e9f
Sonalika Surbhi <164391343+SonalikaSurbhi@users.noreply.github.com>
2024-05-30 16:44:24 +0530

    Delete Task2-Resume_Builder directory

--- Task2-Resume_Builder/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Footer() {
-  return (
-    <div className='w-full h-[60px] bg-fuchsia-900 text-white font-serif text-center tracking-wider text-sm'><p className='pt-3'>@resumeBuilder</p></div>
-  )
-}
-
-export default Footer
